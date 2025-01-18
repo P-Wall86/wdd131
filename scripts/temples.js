f05c2be7d1d2165616e42569ed2ce55069ad7a85
P-Wall86 <sisterwall@gmail.com>
2025-01-17 23:02:50 -0300

    recheck

--- scripts/temples.js
+++ scripts/temples.js
@@ -15,4 +15,14 @@ hamburger.addEventListener('click', () => {
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', ()=> {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
-}))
+
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            // Remove the active class from all links
+            navLinks.forEach(link => link.classList.remove('active'));
+            // Add the active class to the clicked link
+            this.classList.add('active');
+        });
+    });
+}));
