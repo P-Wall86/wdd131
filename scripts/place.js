1f0e884e45ddeae978bf552828d2147e7c028e68
P-Wall86 <sisterwall@gmail.com>
2025-01-23 22:39:16 -0300

    preaudit

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,17 @@ var lastModified = document.lastModified;
 
 document.querySelector("footer p:first-child").innerHTML = "&copy" + currentYear + " 🌴 Pamela Pared 🌴 Argentina ";
 document.querySelector("footer p:nth-child(2)").innerHTML = "Last modified: " + lastModified;
+
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
+  }
+  
+  const temperature = 34;
+  const windSpeed = 10;
+
+  if (temperature <= 10 && windSpeed > 4.8) {
+    const windChill = calculateWindChill(temperature, windSpeed);
+    document.getElementById("wind-chill-value").innerHTML = `Windchill: ${windChill}°C`;
+} else {
+  document.getElementById("wind-chill-value").innerHTML = "N/A";
+}
