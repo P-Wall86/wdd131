5661bfde6d2c2385c71da35da0f9de4352d0e714
P-Wall86 <sisterwall@gmail.com>
2025-01-30 22:23:26 -0300

    lazyload

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,5 @@
+var currentYear = new Date().getFullYear();
+var lastModified = document.lastModified;
+
+document.querySelector("footer p:first-child").innerHTML = "&copy" + currentYear + " - Pamela Pared - Argentina ";
+document.querySelector("footer p:nth-child(2)").innerHTML = "Last modified: " + lastModified;
